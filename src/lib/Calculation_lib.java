c8125b9612a6e841d791fdd5aac64ed07c409038
saodq <saodq@DESKTOP-LLVCPCK>
2017-12-11 17:57:43 +0900

    四則演算の計算

---
+++ src/lib/Calculation_lib.java
@@ -0,0 +1,26 @@
+package lib;
+
+public class Calculation_lib {
+    private int m, n;/*int m,int nはローカル変数*/
+
+	public Calculation_lib(int m, int n) {
+		super();
+		this.m = m;/*thisがついたらフィールド*/
+		this.n = n;
+	}
+    public int getPlus() {
+    	return this.m+this.n;
+    }
+    public int getMinus() {
+    	return this.m-this.n;
+    }
+    public int getTime() {
+    	return this.m*this.n;
+    }
+    public int getDivide() {
+    	return this.m/this.n;
+    }
+    public int getMod() {
+    	return this.m%this.n;
+    }
+}
